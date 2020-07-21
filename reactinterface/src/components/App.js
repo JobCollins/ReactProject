daf95a7d72f6df4526318412dfa2c5344f26df99
JobCollins <dulo.collins@gmail.com>
2020-07-21 20:29:26 +0300

    feature: fetch
    using the fetch function in a lifecycle method to get data

--- reactinterface/src/components/App.js
+++ reactinterface/src/components/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/App.css';
 
 import AddAppointments from './AddAppointments'
 import SearchAppointments from './SearchAppointments'
 import ListAppointments from './ListAppointments'
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <SearchAppointments />
-              <ListAppointments />
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      appointments: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('./data.json')
+    .then(res => res.json())
+    .then(result => {
+      const appointments = result.map(item => {
+        return item;
+      })
+
+      this.setState({
+        appointments : appointments
+      });
+
+    });
+  }
+
+  render(){
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
+  
 }
 
 export default App;
