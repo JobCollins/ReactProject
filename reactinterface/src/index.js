0661ea0cb64681fa6a0d8fb49a3f4006a14c8aaa
JobCollins <dulo.collins@gmail.com>
2020-07-21 19:52:56 +0300

    chore: Added modules changes in dependencies in package.json and imports in index.js

--- reactinterface/src/index.js
+++ reactinterface/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'jquery/dist/jquery'
+import 'popper.js/dist/umd/popper'
+import 'bootstrap/dist/js/bootstrap'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
