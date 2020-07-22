0df1d2939458a5a1d3986513f05c1870e9e8f13b
JobCollins <dulo.collins@gmail.com>
2020-07-22 16:54:06 +0300

    feature: Linking the frontend and backend
    introduced the fetch API to fetch data, added react hooks and fetch to pages

--- blog/src/pages/Article.js
+++ blog/src/pages/Article.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ArticlesList from '../components/ArticlesList'
 import articleContent from './article-content'
 import NotFoundPage from './NotFound'
 
 const Article = ({ match }) => {
+
     const name = match.params.name;
     const article = articleContent.find(
         article => article.name === name
     )
+
+    const [articleInfo, setArticleInfo] = useState({
+        upvotes:0,
+        comments:[]
+    })
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result  = await fetch(`/api/article/${name}`)
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData()
+    }, [name])
 
     if(!article) return <NotFoundPage />
 
@@ -17,6 +32,7 @@ const Article = ({ match }) => {
     return (
         <>
             <h1>{article.title}</h1>
+            <p>This post has been upvoted {articleInfo.upvotes} times</p>
             {article.content.map(
                 (paragraph, key) => (
                     <p key={key}>{paragraph}</p>
