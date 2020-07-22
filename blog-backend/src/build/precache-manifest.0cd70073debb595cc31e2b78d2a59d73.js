f5db654e95c50a56c04f939d6d3b562626a0ac41
JobCollins <dulo.collins@gmail.com>
2020-07-22 17:49:12 +0300

    chore: Preparing the app for release

---
+++ blog-backend/src/build/precache-manifest.0cd70073debb595cc31e2b78d2a59d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eec4ff426142ffcdd4f57ee80cffcec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9336db457a3a5e039823",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "2cf0604cd0c6e44e3578",
+    "url": "/static/js/2.cd74a7c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cd74a7c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9336db457a3a5e039823",
+    "url": "/static/js/main.62793cda.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import { MongoClient } from 'mongodb'
+import path from 'path'
 
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')))
 
 app.use(bodyParser.json())
 
@@ -98,5 +101,9 @@ app.post(
         
     }
 )
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'))
+})
 
 app.listen(8000, () => console.log('Listening on port 8000'))
