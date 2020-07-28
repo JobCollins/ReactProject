b6af057cfd898ccad94f09ce11c8a55b281cf385
JobCollins <dulo.collins@gmail.com>
2020-07-29 01:36:46 +0300

    feature: Banner
    The banner of the app

---
+++ dukani/src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <img 
+                className="home__image"
+                src="https://www.amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Toys_en_US_2x._CB431858162_.jpg" 
+                alt=""
+            />
+        </div>
+    )
+}
+
+export default Home
